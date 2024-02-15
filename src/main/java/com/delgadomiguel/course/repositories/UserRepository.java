27be0872bee75bccdbdb94d8ba6c3ce9618adb47
4Miguel2 <delgdevoleper@gmail.com>
2024-02-15 11:20:24 -0300

    Service layer, component registration

--- src/main/java/com/delgadomiguel/course/repositories/UserRepository.java
+++ src/main/java/com/delgadomiguel/course/repositories/UserRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long > {
 
-    //Nao preciso criar implementações, pois,
+    //Não preciso criar implementações, pois
     // estou passando minha entidade como o tipo genérico e
-    // o spring data jpa já tem implementações as necessárias
+    // o Spring Data JPA já fornece as implementações necessárias.
+
+    //Também não é necessário colocar o @Repository para realizarmos a injeção de dependência
+    // automática, pois o JpaRepository, a interface que estamos estendendo,
+    // já é registrada como componente pelo proprio Spring.
 
 }
--- src/main/java/com/delgadomiguel/course/resources/UserResource.java
+++ src/main/java/com/delgadomiguel/course/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.delgadomiguel.course.resources;
 
 import com.delgadomiguel.course.entities.User;
+import com.delgadomiguel.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "99999", "123123");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}") // Isso significa que a requisição vai aceitar um parâmetro na url, nesse caso um Id
+    public ResponseEntity<User> findById(@PathVariable Long id){ //PathVariable serve para aceitar o parâmetro da url como parâmetro no método
+        User obj = userService.findById(id);
+
+        return ResponseEntity.ok().body(obj);
     }
 
 }
