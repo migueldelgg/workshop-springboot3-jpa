c46c7bc7a821183ef6773038f624e64b1ab06720
4Miguel2 <delgdevoleper@gmail.com>
2024-02-15 23:50:06 -0300

    OrderStatus enum - Manipulando tipo Enum manualmente

--- src/main/java/com/delgadomiguel/course/entities/Order.java
+++ src/main/java/com/delgadomiguel/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.delgadomiguel.course.entities;
 
+import com.delgadomiguel.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -13,51 +14,81 @@ public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    // Significa que esse atributo é a chave primária do banco de dados
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //.Isso gera pode mudar a dependeer do banco de dados.
-    private Long id;                                    //O valor gerado não é aleatório, mas sim sequencial.
+    // Identifica este atributo como a chave primária do banco de dados
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // A estratégia de geração de valor para a chave primária (pode variar dependendo do banco de dados)
+    private Long id;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    // O momento em que o pedido foi realizado
     private Instant moment;
 
-    @ManyToOne //Muitos para um / muitos pedidos para um cliente
-    @JoinColumn(name = "client_id") // nome da chave estrangeira no banco de dados
+    // O código do status do pedido
+    private Integer orderStatus;
+
+    @ManyToOne
+    // Relação muitos-para-um com a entidade User: muitos pedidos para um cliente
+    @JoinColumn(name = "client_id") // Nome da chave estrangeira no banco de dados
     private User client;
 
     public Order() {
-
     }
 
-    public Order(Long id, Instant moment, User client) {
+    // Construtor com todos os atributos
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
+    // Getters e Setters
+
+    // Retorna o ID do pedido
     public Long getId() {
         return id;
     }
 
+    // Define o ID do pedido
     public void setId(Long id) {
         this.id = id;
     }
 
+    // Retorna o momento em que o pedido foi realizado
     public Instant getMoment() {
         return moment;
     }
 
+    // Define o momento em que o pedido foi realizado
     public void setMoment(Instant moment) {
         this.moment = moment;
     }
 
+    // Retorna o cliente associado ao pedido
     public User getClient() {
         return client;
     }
 
+    // Define o cliente associado ao pedido
     public void setClient(User client) {
         this.client = client;
     }
 
+    // Retorna o status do pedido como um enum OrderStatus
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    // Define o status do pedido a partir de um enum OrderStatus
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    // Sobrescrita dos métodos equals e hashCode
+
+    // Compara se dois pedidos são iguais com base no ID
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -66,6 +97,7 @@ public class Order implements Serializable {
         return Objects.equals(id, order.id);
     }
 
+    // Gera um código de hash para o pedido com base no ID
     @Override
     public int hashCode() {
         return Objects.hash(id);
---
+++ src/main/java/com/delgadomiguel/course/entities/enums/OrderStatus.java
@@ -0,0 +1,33 @@
+package com.delgadomiguel.course.entities.enums;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+
+    private int code;
+
+    private OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    //Retorna o enum OrderStatus correspondente ao código fornecido.
+    //Percorre todos os valores possíveis do tipo enum e verifica se o código fornecido corresponde a algum deles.
+    //Se um valor correspondente for encontrado, retorna o OrderStatus equivalente; caso contrário, retorna null.
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code.");
+    }
+
+}
